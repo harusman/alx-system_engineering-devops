6d22325128ec89a55e46026cb42b13ff3914603b
harusman <westamder32@gmail.com>
2022-11-15 22:17:35 -0800

    0x6

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 
-Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-List = [Format[0].compact, Format[1].compact, Format[2].compact]
-puts List.join(',')
-
-#OR
-
-#puts ARGV[0]
-#  .scan(/(?<=from:|to:|flags:).*?(?=\])/)
-# .join(',')
+from = ARGV[0].scan(/from:(.*?)\]/)
+to = ARGV[0].scan(/to:(.*?)\]/)
+flags = ARGV[0].scan(/flags:(.*?)\]/)
+puts [from, to, flags].join(',')
